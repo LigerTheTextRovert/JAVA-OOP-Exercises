b894a189eef021f104baf447ea9f759bb3b45b99
LiGER <samarghandi112w34@gmail.com>
2025-04-15 17:45:48 +0330

    Product class is done!

---
+++ 8.OnlineShoppingSystem/Product.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+public class Product {
+    private String productName;
+    private int id;
+    private float productPrice;
+    private int stockCount;
+
+    public Product (String name, float price){
+        Random rand = new Random();
+        this.id = rand.nextInt(50000) + 10000;
+        this.productName = name;
+        this.productPrice = price;
+    }
+
+    //----------------------------------------------------------------------------
+    // GETTERS
+    //----------------------------------------------------------------------------
+    public int getId (){
+        return id;
+    }
+    public String getProductName(){
+        return productName;
+    }
+    public float getProductPrice(){
+       return productPrice;
+    }
+}
